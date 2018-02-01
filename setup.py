c620ba068d78c6f96702b289ec713c24aad054d4
Christophe Bedetti <christophe.bedetti@gmail.com>
2018-02-01 18:59:21 -0500

    Add singularity recipe

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 DISTNAME = "dcm2bids"
 DESCRIPTION = description
-VERSION = "1.1.4"
+VERSION = "1.1.5"
 AUTHOR = "Christophe Bedetti"
 AUTHOR_EMAIL = "christophe.bedetti@umontreal.ca"
 URL = "https://github.com/cbedetti/Dcm2Bids"
