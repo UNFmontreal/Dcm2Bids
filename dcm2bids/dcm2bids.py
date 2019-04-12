4bb2a9e9bd4763451964bfc2e1e8c094dfeeb85c
arnaudbore <arnaud.bore@gmail.com>
2019-04-12 09:17:52 -0400

    flake8

--- dcm2bids/dcm2bids.py
+++ dcm2bids/dcm2bids.py
@@ -38,7 +38,8 @@ class Dcm2bids(object):
     """
 
     def __init__(
-            self, dicom_dir, config, participant=DEFAULT.participant, output_dir=DEFAULT.outputDir,
+            self, dicom_dir, config, participant=DEFAULT.participant,
+            output_dir=DEFAULT.outputDir,
             session=DEFAULT.session, clobber=DEFAULT.clobber,
             forceDcm2niix=DEFAULT.forceDcm2niix, log_level=DEFAULT.logLevel,
             **_):
@@ -52,13 +53,13 @@ class Dcm2bids(object):
         self.forceDcm2niix = forceDcm2niix
         self.logLevel = log_level
 
-        #logging setup
+        # logging setup
         self.set_logger()
 
         self.logger.info("--- dcm2bids start ---")
         self.logger.info("OS:version: {}".format(platform.platform()))
         self.logger.info("python:version: {}".format(
-            sys.version.replace("\n","")))
+            sys.version.replace("\n", "")))
         self.logger.info("dcm2bids:version: {}".format(__version__))
         self.logger.info("dcm2niix:version: {}".format(dcm2niix_version()))
         self.logger.info("participant: {}".format(self.participant.name))
@@ -67,18 +68,16 @@ class Dcm2bids(object):
         self.logger.info(
                 "BIDS directory: {}".format(os.path.realpath(output_dir)))
 
-
     @property
     def dicomDirs(self):
         return self._dicomDirs
-
 
     @dicomDirs.setter
     def dicomDirs(self, value):
         if isinstance(value, list):
             self._dicomDirs = value
         else:
-            self._dicomDirs = [value,]
+            self._dicomDirs = [value, ]
 
     def set_logger(self):
         """ Set a basic logger"""
@@ -86,8 +85,8 @@ class Dcm2bids(object):
         logFile = os.path.join(logDir, "{}_{}.log".format(
                 self.participant.prefix, datetime.now().isoformat()))
 
-        #os.makedirs(logdir, exist_ok=True)
-        #python2 compatibility
+        # os.makedirs(logdir, exist_ok=True)
+        # python2 compatibility
         if not os.path.exists(logDir):
             os.makedirs(logDir)
 
@@ -99,11 +98,13 @@ class Dcm2bids(object):
         session = self.participant.session
         participant = self.participant.name
 
-        participant = re.match(self.config["participant"]["expression"], sidecar._data[self.config["participant"]["dcmTag"]])
+        participant = re.match(self.config["participant"]["expression"],
+                               sidecar._data[self.config["participant"]["dcmTag"]])
         participant = participant.groups()[0]
 
         if "session" in self.config:
-            session = re.match(self.config["session"]["expression"], sidecar._data[self.config["session"]["dcmTag"]])
+            session = re.match(self.config["session"]["expression"],
+                               sidecar._data[self.config["session"]["dcmTag"]])
             session = session.groups()[0]
 
         self.participant = Participant(participant, session)
@@ -112,11 +113,16 @@ class Dcm2bids(object):
         """
         """
         if not self.participant.name:
-            dcm2niix = Dcm2niix(self.dicomDirs, self.bidsDir, "",
-                    self.config.get("dcm2niixOptions", DEFAULT.dcm2niixOptionsNA))
+            dcm2niix = Dcm2niix(self.dicomDirs,
+                                self.bidsDir,
+                                "",
+                                self.config.get("dcm2niixOptions",
+                                                DEFAULT.dcm2niixOptionsNA))
         else:
-            dcm2niix = Dcm2niix(self.dicomDirs, self.bidsDir, self.participant,
-                            self.config.get("dcm2niixOptions", DEFAULT.dcm2niixOptions))
+            dcm2niix = Dcm2niix(self.dicomDirs,
+                                self.bidsDir,
+                                self.participant,
+                                self.config.get("dcm2niixOptions", DEFAULT.dcm2niixOptions))
 
         dcm2niix.run(self.forceDcm2niix)
 
@@ -129,8 +135,10 @@ class Dcm2bids(object):
         if not self.participant.name and "participant" in self.config:
             self.setParticipant(sidecars[0])
 
-        parser = SidecarPairing(sidecars, self.config["descriptions"],
-                self.config.get("searchMethod", DEFAULT.searchMethod))
+        parser = SidecarPairing(sidecars,
+                                self.config["descriptions"],
+                                self.config.get("searchMethod",
+                                                DEFAULT.searchMethod))
         parser.build_graph()
         parser.build_acquisitions(self.participant)
         parser.find_runs()
@@ -144,7 +152,6 @@ class Dcm2bids(object):
 
         return os.EX_OK
 
-
     def move(self, acquisition):
         """
         """
@@ -152,12 +159,12 @@ class Dcm2bids(object):
             root, ext = splitext_(srcFile)
             dstFile = os.path.join(self.bidsDir, acquisition.dstRoot + ext)
 
-            #os.makedirs(os.path.dirname(dstFile), exist_ok=True)
-            #python2 compatibility
+            # os.makedirs(os.path.dirname(dstFile), exist_ok=True)
+            # python2 compatibility
             if not os.path.exists(os.path.dirname(dstFile)):
                 os.makedirs(os.path.dirname(dstFile))
 
-            #checking if destination file exists
+            # checking if destination file exists
             if os.path.isfile(dstFile):
                 self.logger.info("'{}' already exists".format(dstFile))
 
@@ -168,10 +175,10 @@ class Dcm2bids(object):
                     self.logger.info("Use clobber option to overwrite")
                     continue
 
-            #it's an anat nifti file and the user using a deface script
+            # it's an anat nifti file and the user using a deface script
             if (
                     self.config.get("defaceTpl")
-                    and acquisition.dataType=="anat"
+                    and acquisition.dataType == "anat"
                     and ".nii" in ext):
                 try:
                     os.remove(dstFile)
@@ -181,12 +188,12 @@ class Dcm2bids(object):
                 cmd = defaceTpl.format(srcFile=srcFile, dstFile=dstFile)
                 run_shell_command(cmd)
 
-            #use
+            # use
             elif ext == ".json":
                 data = acquisition.dstSidecarData(self.config["descriptions"])
                 save_json(dstFile, data)
                 os.remove(srcFile)
 
-            #just move
+            # just move
             else:
                 os.rename(srcFile, dstFile)
