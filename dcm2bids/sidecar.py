56b9e5e891ef6526913c654c04d467ed599dc7b5
Lennart Walger <lennart.walger@ukbonn.de>
2021-03-02 17:57:43 +0100

    moved name to string conversion outside if statement

--- dcm2bids/sidecar.py
+++ dcm2bids/sidecar.py
@@ -175,10 +175,10 @@ class SidecarPairing(object):
         """
 
         def compare(name, pattern):
+            name = str(name)
             if self.searchMethod == "re":
                 return bool(re.search(pattern, name))
             else:
-                name = str(name)
                 pattern = str(pattern)
                 if not self.caseSensitive:
                     name = name.lower()
