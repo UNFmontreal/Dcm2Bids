17fabaafeafedca2fa6bc23138ebae333d2647f3
arnaudbore <arnaudbore@gmail.com>
2022-04-19 08:43:28 -0400

    fix_dcm2bids_helper

--- dcm2bids/utils.py
+++ dcm2bids/utils.py
@@ -5,8 +5,9 @@ import csv
 import json
 import logging
 import os
-import shlex
 from collections import OrderedDict
+import shlex
+import shutil
 from subprocess import check_output
 
 
@@ -117,3 +118,44 @@ def run_shell_command(commandLine):
     logger = logging.getLogger(__name__)
     logger.info("Running %s", commandLine)
     return check_output(shlex.split(commandLine))
+
+
+def assert_dirs_empty(parser, args, required):
+    """
+    Assert that all directories exist are empty.
+    If exists and not empty, and -f used, delete dirs.
+
+    Parameters
+    ----------
+    parser: argparse.ArgumentParser object
+        Parser.
+    args: argparse namespace
+        Argument list.
+    required: string or list of paths to files
+        Required paths to be checked.
+    create_dir: bool
+        If true, create the directory if it does not exist.
+    """
+    def check(path):
+        if os.listdir(path):
+            if not args.overwrite:
+                parser.error(
+                    'Output directory {} isn\'t empty and some files could be '
+                    'overwritten or even deleted. Use -f option if you want '
+                    'to continue.'.format(path))
+            else:
+                for the_file in os.listdir(path):
+                    file_path = os.path.join(path, the_file)
+                    try:
+                        if os.path.isfile(file_path):
+                            os.unlink(file_path)
+                        elif os.path.isdir(file_path):
+                            shutil.rmtree(file_path)
+                    except Exception as e:
+                        print(e)
+
+    if isinstance(required, str):
+        required = [required]
+
+    for cur_dir in required:
+        check(cur_dir)
