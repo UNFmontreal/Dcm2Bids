a3b3e9b17d179ee16ef4c0319f014ceead5e12ea
Samuel Guay <samuel.guay@umontreal.ca>
2022-04-22 08:26:33 -0400

    fix typos

--- dcm2bids/structure.py
+++ dcm2bids/structure.py
@@ -255,8 +255,8 @@ class Acquisition(object):
 
         if len(suffix_list) != 1:
             self.logger.warning("There was more than one suffix found "
-                                f"({suffix_list}). this is not BIDS "
-                                "compliant. Make sure you know what"
+                                f"({suffix_list}). This is not BIDS "
+                                "compliant. Make sure you know what "
                                 "you are doing.")
 
         if current_name != new_name:
