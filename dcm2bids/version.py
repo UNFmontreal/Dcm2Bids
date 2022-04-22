1df93a1bbc0537e079a809a4ffe0eedeefb0e677
arnaudbore <arnaudbore@gmail.com>
2022-04-22 14:22:24 -0400

    update version

--- dcm2bids/version.py
+++ dcm2bids/version.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
-_version_major = 2
-_version_minor = 1
-_version_micro = 6
+_version_major = 3
+_version_minor = 0
+_version_micro = 0
 _version_extra = ''
 
 # Construct full version string from these.
