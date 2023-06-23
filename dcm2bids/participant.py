a53da60d995f73536379ab73a5af8120744a3e7b
arnaud.bore <arnaud.bore@gmail.com>
2023-06-23 12:54:18 -0400

    fix participant

--- dcm2bids/participant.py
+++ dcm2bids/participant.py
@@ -34,10 +34,13 @@ class Participant(object):
     def name(self, name):
         """ Prepend 'sub-' if necessary"""
         if name.startswith("sub-"):
-            self._name = name
-
+            self._name = name   
         else:
             self._name = "sub-" + name
+
+        if '_' in self._name or '-' in self._name.replace('sub-', ''):
+            raise NameError(f"Participant '{self._name}' contains forbidden character "
+                             "(multiple - or _).")
 
     @property
     def session(self):
