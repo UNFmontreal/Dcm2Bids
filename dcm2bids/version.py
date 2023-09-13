4a69347545926178f52944a53e6804b89eda9247
Arnaud Bore <arnaud.bore@gmail.com>
2023-09-13 16:50:59 -0400

    Update version.py
    mandatory micro version

--- dcm2bids/version.py
+++ dcm2bids/version.py
@@ -7,9 +7,7 @@ _version_micro = 0
 _version_extra = ''
 
 # Construct full version string from these.
-_ver = [_version_major, _version_minor]
-if _version_micro:
-    _ver.append(_version_micro)
+_ver = [_version_major, _version_minor, _version_micro]
 if _version_extra:
     _ver.append(_version_extra)
 
