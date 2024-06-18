9ae71ffaf6a7228edb1d2c70982d785031f10ee2
arnaudbore <arnaud.bore@gmail.com>
2024-06-17 20:56:03 -0400

    add a way to show error or warning during dcm2niix execution

--- dcm2bids/dcm2niix_gen.py
+++ dcm2bids/dcm2niix_gen.py
@@ -142,7 +142,11 @@ class Dcm2niixGen(object):
                     shutil.rmtree(self.rm_tmp_dir)
                     self.logger.info("Temporary dicom directory removed.")
 
-                self.logger.debug(f"\n{output}")
+                if "Warning" in output or "Error" in output:
+                    self.logger.info(f"\n{output}")
+                else:
+                    self.logger.debug(f"\n{output}")
+
                 self.logger.info("Check log file for dcm2niix output\n")
 
         else:
